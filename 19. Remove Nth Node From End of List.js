3f79567a982b0ea0a9eb853716d790e56a62d98e
Araxq <kachkovskiy@phystech.edu>
2018-10-01 21:52:42 +0300

    19

--- 19. Remove Nth Node From End of List.js
+++ 19. Remove Nth Node From End of List.js
@@ -10,6 +10,22 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
+const removeNthFromEnd = function (head, n) {
+    let current_node = head;
+    let nth_node = head;
+    let node_before_nth = head;
+    let i = 0;
+    while (current_node.next !== null) {
+        i++;
+        current_node = current_node.next;
+        if (i >= n) {
+            nth_node = nth_node.next;
+            if (i > n) node_before_nth = node_before_nth.next;
+        }
+
+    }
+    if (node_before_nth === nth_node) return head.next;
+    node_before_nth.next = nth_node.next;
+    return head;
 
 };
